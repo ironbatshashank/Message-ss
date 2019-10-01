e1ad82c6e7573ee30ffdc6f37237e24829858f75
Shashank Srivastava <shashanksri65@gmail.com>
2019-10-01 15:59:57 +0530

    Changes dependencies

--- app/src/main/java/com/example/message_ss/FindUserActivity.java
+++ app/src/main/java/com/example/message_ss/FindUserActivity.java
@@ -9,5 +9,7 @@ public class FindUserActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_user);
+
+
     }
 }
