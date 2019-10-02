fecfec8286cf2d71fcae16a1065e433efa7725f8
Shashank Srivastava <shashanksri65@gmail.com>
2019-10-02 20:06:19 +0530

    Layout Manager

--- app/src/main/java/com/example/message_ss/FindUserActivity.java
+++ app/src/main/java/com/example/message_ss/FindUserActivity.java
@@ -1,8 +1,10 @@
 package com.example.message_ss;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class FindUserActivity extends AppCompatActivity {
@@ -17,7 +19,14 @@ public class FindUserActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_user);
 
+        initializeRecyclerView();
+    }
 
-
+    private void initializeRecyclerView() {
+        mUserList = findViewById(R.id.userList);
+        mUserList.setNestedScrollingEnabled(false);
+        mUserList.setHasFixedSize(false);
+        mUserListLayoutManager = new LinearLayoutManager(getApplicationContext(), LinearLayout.VERTICAL, false);
+        mUserListLayoutManager(mUserListLayoutManager);
     }
 }
