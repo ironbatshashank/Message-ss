99ed140920e7bc89714e69570c366ca7ada26f32
Shashank Srivastava <shashanksri65@gmail.com>
2019-10-02 17:46:20 +0530

    User activity display

--- app/src/main/java/com/example/message_ss/FindUserActivity.java
+++ app/src/main/java/com/example/message_ss/FindUserActivity.java
@@ -3,13 +3,20 @@ package com.example.message_ss;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class FindUserActivity extends AppCompatActivity {
+
+
+    private RecyclerView mUserList;
+    private RecyclerView.Adapter mUserListAdapter;
+    private RecyclerView.LayoutManager mUserListLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_user);
+
 
 
     }
--- app/src/main/java/com/example/message_ss/MainActivity.java
+++ app/src/main/java/com/example/message_ss/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.message_ss;
 
 import android.content.Intent;
 import android.os.Bundle;
+/*
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.NonNull;
+*/
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
