79d50fc05670caa23b45c1750af26cba46680284
Shashank Srivastava <shashanksri65@gmail.com>
2019-10-03 10:50:32 +0530

    User object

---
+++ app/src/main/java/com/example/message_ss/UserListAdapter.java
@@ -0,0 +1,6 @@
+package com.example.message_ss;
+
+public class UserListAdapter {
+
+    
+}
---
+++ app/src/main/java/com/example/message_ss/UserObject.java
@@ -0,0 +1,15 @@
+package com.example.message_ss;
+
+public class UserObject {
+
+    private String name, phone;
+
+    public UserObject(String name, String phone){
+
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public String getName()    { return name; }
+    public String getPhone()    { return phone; }
+}
