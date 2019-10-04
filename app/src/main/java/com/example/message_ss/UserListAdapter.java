7534f86265f6f05ff6c40b092709170dc309e8c6
Shashank Srivastava <shashanksri65@gmail.com>
2019-10-04 19:55:50 +0530

    user items

--- app/src/main/java/com/example/message_ss/UserListAdapter.java
+++ app/src/main/java/com/example/message_ss/UserListAdapter.java
@@ -1,14 +1,26 @@
 package com.example.message_ss;
 
+import android.view.View;
+
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
-public class UserListAdapter implements RecyclerView.Adapter<UserListAdapter.UserListRecyclerView> {
+public class UserListAdapter implements RecyclerView.Adapter<UserListAdapter.UserListViewHolder> {
 
     ArrayList<UserObject> userList;
 
     public UserListAdapter(ArrayList<UserObject> userList) {
         this.userList = userList;
     }
+
+
+
+    public class UserListViewHolder extends RecyclerView.ViewHolder{
+
+        public UserListViewHolder(View view){
+            super(view);
+        }
+    }
+
 }
