65228dcb5ef8aca37e01d78ebae0cab7f88d9a42
Shashank Srivastava <shashanksri65@gmail.com>
2019-10-11 04:58:01 +0530

    minorbugs

--- app/src/main/java/com/example/message_ss/UserListAdapter.java
+++ app/src/main/java/com/example/message_ss/UserListAdapter.java
@@ -1,7 +1,12 @@
 package com.example.message_ss;
 
+import android.text.Layout;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -14,12 +19,32 @@ public class UserListAdapter implements RecyclerView.Adapter<UserListAdapter.Use
         this.userList = userList;
     }
 
+    @NonNull
+    @Override
+    public UserListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View layoutView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_user,null,false);
+        RecyclerView.LayoutParams; 
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UserListViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return userList.size();
+    }
 
 
     public class UserListViewHolder extends RecyclerView.ViewHolder{
 
+        public TextView mName, mPhone;
         public UserListViewHolder(View view){
             super(view);
+            mName = view.findViewById(R.id.name);
+            mPhone = view.findViewById(R.id.phone);
         }
     }
 
